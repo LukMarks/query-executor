0d34b9926799a9dea069161159389ec96e080111
LukMarks <lucas.mmoreno@hotmail.com>
2024-09-03 14:58:29 -0300

    feat: query metadata file

--- src/main/java/org/queryExecuter/QueryExecutor.java
+++ src/main/java/org/queryExecuter/QueryExecutor.java
@@ -1,14 +1,22 @@
 package org.queryExecuter;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.opencsv.CSVWriter;
+import org.json.JSONObject;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QueryExecutor {
 
@@ -18,6 +26,8 @@ public class QueryExecutor {
     private static QuerySettings querySetting;
 
     private static Connection databaseConnection;
+
+    private static Map<String, String> resultMetadata;
 
     public static void main(String[] queryFilePath) throws Throwable{
 
@@ -51,9 +61,14 @@ public class QueryExecutor {
     public static void saveQueryResult(ResultSet queryResult, String dataFile, char csvSeparator ) {
 
         try {
+
             CSVWriter csvWriter = new CSVWriter(new FileWriter(dataFile+".csv"), csvSeparator);
             csvWriter.writeAll(queryResult, true);
             csvWriter.close();
+
+            ResultSetMetaData queryMetadata=queryResult.getMetaData();
+            saveMetadata(queryMetadata, dataFile);
+
         } catch (IOException e) {
             System.err.println(e.getMessage());
         } catch (SQLException e) {
@@ -68,8 +83,32 @@ public class QueryExecutor {
         errorMessageFile.close();
     }
 
-    public static void saveMetadata(ResultSet queryResult, String metadaFile){
 
+    public static void saveMetadata(ResultSetMetaData queryResultMetadata, String dataFile){
+
+        try {
+            Map<String, String> resultMetadata = buildMetadataMap(queryResultMetadata);
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+            writer.writeValue(new File(dataFile+"-metadata.json"), resultMetadata);
+
+
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Map<String, String> buildMetadataMap(ResultSetMetaData queryMetadata) throws SQLException {
+        Map<String, String> columnsMetadata = new HashMap<>();
+        for (int column = 1; column <= queryMetadata.getColumnCount(); column++) {
+            columnsMetadata.put(queryMetadata.getColumnName(column),
+                               queryMetadata.getColumnTypeName(column));
+        }
+
+        return columnsMetadata;
     }
 
 
